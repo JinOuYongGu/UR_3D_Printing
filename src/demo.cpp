3c75fcc2f61cad8cf0394286cebc36226baffd2e
JinOu <691422759@qq.com>
2020-07-07 16:50:42 +0800

    Init create

--- src/demo.cpp
+++ src/demo.cpp
@@ -6,8 +6,11 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
+    // Standard C++ lib
     cout << "hello world!" << endl;
+
+    // ROS lib method
     ROS_INFO("hello world! --from Ros");
-    
+
     return 0;
 }
